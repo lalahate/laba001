26048f3177371636e7769058f4928e2499ea67cb
Eliza <d1225zea@rb.asu.ru>
2023-12-13 19:08:45 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+app = FastAPI()
+
+
+@app.get("/")
+async def p_index():
+    return {"FIO": "Жерносенко Елизавета Александровна"}
+
+
+@app.get("/users",response_class=HTMLResponse)
+async def p_users():
+    output ="<h3> Номер: 89628023376 </h3>"
+    return output
+
+@app.get("/tools",response_class=HTMLResponse)
+async def p_tools():
+    output = "<h2> <center> Artist </center></h2>"
+    return output
+
